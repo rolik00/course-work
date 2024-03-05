fdc0d1d4c3d038398e702f207c43e30c89678530
rolik00 <148611487+rolik00@users.noreply.github.com>
2024-03-05 21:19:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new MainWindow();
-            }
-        });
+        MainWindow mw = new MainWindow();
     }
 }
