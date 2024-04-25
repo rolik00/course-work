5dc191815b6bbe8e1e174763a0fd5b94b33a4afa
rolik00 <148611487+rolik00@users.noreply.github.com>
2024-04-25 12:27:20 +0300

    Update Connection.java

--- Connection.java
+++ Connection.java
@@ -1,96 +1,221 @@
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class Connection {
-    public String getQuestion(int theme, int number) {
-        final String connectionUrl = "jdbc:sqlite:coursework.db";
-        int resnumber = number+(theme-1)*20;
+    public void create_test_info(int topic) {
+        Set<Integer> number_questions = generate_test_numbers(topic);
+        int i = 0;
+        for (int number : number_questions)
+        {
+            TestWindow.questions[i] = get_question(number);
+            TestWindow.Type[i] = get_type(number);
+            ArrayList<String> answers = get_answers(number);
+            if (TestWindow.Type[i] == 5) {
+                int n = answers.size() / 2;
+                TestWindow.columns[i] = new String[n];
+                String [] tmp = new String[n + 1];
+                tmp[0] = "";
+                int size = 1;
+                for (int j = 0; j < answers.size(); j++)
+                {
+                    if (j < n) TestWindow.columns[i][j] = answers.get(j);
+                    else if (!answers.get(j).equals(""))
+                    {
+                        tmp[j - n + 1] = answers.get(j);
+                        size++;
+                    }
+                }
+                TestWindow.answers[i] = new String[size];
+                for (int j = 0; j < size; j++)
+                {
+                    TestWindow.answers[i][j] = tmp[j];
+                }
+            }
+            else
+            {
+                TestWindow.answers[i] = tranform(answers);
+            }
+            ArrayList<String> right_answers = get_right_answers(number);
+            TestWindow.right_answers[i] = tranform(right_answers);
+            i++;
+        }
+    }
+
+    private Set<Integer> generate_test_numbers(int topic) {
+        Set<Integer> st = new HashSet<Integer>();
+        Random r = new Random();
+        while (st.size() != 10) {
+            int x;
+            if (topic != 5) {
+                x = r.nextInt(20) + 1;
+                x = x + topic * 20;
+            } else {
+                x = r.nextInt(100) + 1;
+            }
+            st.add(x);
+        }
+        return st;
+    }
+
+    private String get_question(int number) {
+        java.sql.Connection conn = null;
+        final String url = "jdbc:sqlite:coursework.db";
         try {
-            java.sql.Connection con = DriverManager.getConnection(connectionUrl);
-            Statement stmt  = con.createStatement();
-            try (ResultSet rs = stmt.executeQuery("SELECT name FROM questions WHERE id = "+resnumber)) {
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            try (ResultSet rs = stmt.executeQuery("SELECT name FROM questions WHERE id = " + number)) {
                 rs.next();
                 return rs.getString("name");
             }
+
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
-        return null;
+        return "";
     }
 
-    public int getType(int theme, int number) {
-        final String connectionUrl = "jdbc:sqlite:coursework.db";
-        int resnumber = number+(theme-1)*20;
+    private int get_type(int number) {
+        java.sql.Connection conn = null;
+        final String url = "jdbc:sqlite:coursework.db";
         try {
-            java.sql.Connection con = DriverManager.getConnection(connectionUrl);
-            Statement stmt  = con.createStatement();
-            try (ResultSet rs = stmt.executeQuery("SELECT type FROM questions WHERE id = "+resnumber)) {
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            try (ResultSet rs = stmt.executeQuery("SELECT type FROM questions WHERE id = " + number)) {
                 rs.next();
                 return rs.getInt("type");
             }
+
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
         return -1;
     }
 
-    public String getRightVar(int theme, int number) {
-        final String connectionUrl = "jdbc:sqlite:coursework.db";
-        int resnumber = number+(theme-1)*20;
+    private ArrayList<String> get_answers(int number) {
+        java.sql.Connection conn = null;
+        final String url = "jdbc:sqlite:coursework.db";
+        ArrayList<String> answers = new ArrayList<>();
         try {
-            java.sql.Connection con = DriverManager.getConnection(connectionUrl);
-            Statement stmt  = con.createStatement();
-            try (ResultSet rs = stmt.executeQuery("SELECT name FROM rightvar WHERE idquestion = "+resnumber)) {
-                if (getType(theme,number)==2){
-                    String res = "";
-                    while (rs.next())
-                        res+=rs.getString("name");
-                    return res;
-                }
-                else{
-                    rs.next();
-                    return rs.getString("name");}
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public ArrayList <String> getAns(int theme, int number) {
-        final String connectionUrl = "jdbc:sqlite:coursework.db";
-        int resnumber = number+(theme-1)*20;
-        ArrayList <String> res = new ArrayList<>();
-        try {
-            java.sql.Connection con = DriverManager.getConnection(connectionUrl);
-            Statement stmt  = con.createStatement();
-            try (ResultSet rs = stmt.executeQuery("SELECT name FROM variants WHERE idquestion = "+resnumber)) {
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            try (ResultSet rs = stmt.executeQuery("SELECT name FROM variants WHERE idquestion = " + number)) {
                 while (rs.next()) {
-                    res.add(rs.getString("name"));
+                    answers.add(rs.getString("name"));
                 }
-                return res;
             }
+
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
-        return res;
+        return answers;
     }
 
-    public int getStatistics(int theme) {
-        final String connectionUrl = "jdbc:sqlite:coursework.db";
+    private ArrayList<String> get_right_answers(int number) {
+        java.sql.Connection conn = null;
+        final String url = "jdbc:sqlite:coursework.db";
+        ArrayList<String> right_answers = new ArrayList<>();
         try {
-            java.sql.Connection con = DriverManager.getConnection(connectionUrl);
-            Statement stmt  = con.createStatement();
-            try (ResultSet rs = stmt.executeQuery("SELECT result FROM theories WHERE id = "+theme)) {
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            try (ResultSet rs = stmt.executeQuery("SELECT name FROM rightvar WHERE idquestion = " + number)) {
+                while (rs.next()) {
+                    right_answers.add(rs.getString("name"));
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return right_answers;
+    }
+
+    private String[] tranform(ArrayList<String> list) {
+        String[] arr = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
+    public int get_statistics(int topic) {
+        java.sql.Connection conn = null;
+        final String url = "jdbc:sqlite:coursework.db";
+        try {
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            try (ResultSet rs = stmt.executeQuery("SELECT result FROM theories WHERE id = " + topic)) {
                 rs.next();
                 return rs.getInt("result");
             }
+
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
         return -1;
+    }
+
+    public void set_statistics(int topic, int result)
+    {
+        java.sql.Connection conn = null;
+        final String url = "jdbc:sqlite:coursework.db";
+        try {
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("UPDATE theories SET result = " + result + " WHERE id = " + (topic + 1));
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
